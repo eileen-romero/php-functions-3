9e0a45ce5cf71960e904df6119919b305554391a
eileen-romero <paperfox13@Eileens-MacBook-Air.local>
2017-04-04 16:53:37 -0700

    Changing to activities
    Coffee haunts this page

--- functions.php
+++ functions.php
@@ -10,23 +10,23 @@ function capfirst($word) {
   return $word;
 }
 
-function makeCoffee($theName, $theDrinks, $theCoffee, $theQuantity) {
-  if ($theCoffee != 'nothing' && $theQuantity > 0) {
+function doSomething($theName, $theArray, $theActivity, $theQuantity) {
+  if ($theActivity != 'nothing' && $theQuantity > 0) {
     $valid = true;
-    $price = $theDrinks[$theCoffee];
-    $total = $price * $theQuantity;
+    $calories = $theArray[$theActivity];
+    $total = $calories * $theQuantity;
     if ($theQuantity < 2) {
-      $title = titlecase($theCoffee).' for '.$theName;
-      $theTotal = 'Total: $'.number_format($total, 2);
-      $description = $theName.' ordered '.$theQuantity.' '.$theCoffee.'.';
+      $title = titlecase($theActivity).' for '.$theName;
+      $theTotal = 'Total Calories Burned:'.number_format($total, 0);
+      $description = $theName.' did '.$theQuantity.' hours of '.$theActivity.'.';
     } elseif ($theQuantity > 50) {
-      $title = 'No '.titlecase($theCoffee).' for '.$theName;
-      $theCoffee = 'rediculous';
-      $description = 'Don&rsquo;t be rediculous, '.$theName.', that&rsquo;s more coffee than any one barista can ever brew on time! Also, you don&rsquo;t have $'.number_format($total, 2).'!';
+      $title = 'No '.titlecase($theActivity).' for '.$theName;
+      $theActivity = 'rediculous';
+      $description = 'Don&rsquo;t be ridiculous, '.$theName.', that&rsquo;s more '.$activity.'than any one person can ever do! Also, you don&rsquo;t have any Energy!';
     } else {
-      $title = titlecase($theCoffee).' for '.$theName;
+      $title = titlecase($theActivity).' for '.$theName;
       $theTotal = 'Total: $'.number_format($total, 2);
-      $description = $theName.' ordered '.number_format($theQuantity).' '.$theCoffee.'s.';
+      $description = $theName.' ordered '.number_format($theQuantity).' '.$theActivity.'s.';
     }
   } else {
     $valid = false;
@@ -35,7 +35,7 @@ function makeCoffee($theName, $theDrinks, $theCoffee, $theQuantity) {
   if ($valid == true) {
     return('
       <div class="card my-4 mx-auto" style="width: 20rem;">
-        <img class="img-fluid" src="images/'.$theCoffee.'.jpg" alt="Card image cap">
+        <img class="img-fluid" src="images/'.$theActivity.'.jpg" alt="Card image cap">
         <div class="card-block">
           <h2 class="h4 card-title">'.$title.'</h2>
           <p class="card-text">'.$description.'</p>
--- index.php
+++ index.php
@@ -3,25 +3,26 @@
   require_once('functions.php');
 
   // Create coffee array with prices
-  $theDrinks = array(
-    'coffee'=>1.25,
-    'espresso'=>2.5,
-    'latte'=>4.25,
-    'mocha'=>4.75
+  $theAdventures = array(
+    'surfing'=>400,
+    'diving'=>250,
+    'skateboarding'=>398,
+    'falling'=>83
   );
 
   // Process the form request
   if( isset($_POST['submit']) )
   {
       $theName = htmlentities($_POST['name']);
-      $theCoffee = htmlentities($_POST['coffee']);
-      $theCoffee = strtolower($theCoffee); // Lowercase form submission
-      $theQuantity = htmlentities($_POST['quantity']);
-      $coffee = makeCoffee($theName, $theDrinks, $theCoffee, $theQuantity);
+      //coffee is the only thing that works. tried to rename this but it wasnt working.
+      $theActivity = htmlentities($_POST['coffee']);
+      $theActivity = strtolower($theActivity); // Lowercase form submission
+      $hours = htmlentities($_POST['quantity']);
+      $activity = doSomething($theName, $theAdventures, $theActivity, $hours);
   } else {
     // User hasn't entered a value
-    $theCoffee = '';
-    $coffee = '';
+    $theActivity = '';
+    $activity = '';
     $theName = '';
   }
 ?>
@@ -30,27 +31,27 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>Functional Coffee</title>
+    <title>Functional activity</title>
     <link rel="stylesheet" href="css/bootstrap.css">
   </head>
   <body class="bg-faded">
     <main class="container py-4">
-      <h1 class="pb-4 font-weight-bold text-center">What Will You Drink?</h1>
+      <h1 class="pb-4 font-weight-bold text-center">What Will You Do?</h1>
       <form class="form-inline justify-content-center" action="" method="post">
         <label for="name" class="sr-only">Name</label>
-        <input class="form-control mr-2" type="text" value="<?php echo ( $theName ? $theName : '' );?>" placeholder="Name" name="name" id="name"> <span class="mr-2">wants a</span>
+        <input class="form-control mr-2" type="text" value="<?php echo ( $theName ? $theName : '' );?>" placeholder="Name" name="name" id="name"> <span class="mr-2">wants to</span>
         <select class="custom-select mr-2" name="coffee" id="coffee">
-          <?php echo '<option value="'.( $theCoffee ? $theCoffee : 'nothing' ).'">'.( $theCoffee ? capfirst($theCoffee) : 'Select a Drink' ).'</option>'; ?>
-          <?php foreach ($theDrinks as $drink => $price) {
-            echo '<option value="'.$drink.'">'.capfirst($drink).'</option>';
+          <?php echo '<option value="'.( $theActivity ? $theActivity : 'nothing' ).'">'.( $theActivity ? capfirst($theActivity) : 'Select an activity' ).'</option>'; ?>
+          <?php foreach ($theAdventures as $action => $burn) {
+            echo '<option value="'.$action.'">'.capfirst($action).'</option>';
           }; ?>
         </select> <span class="mr-2">for</span>
-        <input class="form-control mr-2" type="number" value="<?php echo ( $theQuantity ? $theQuantity : '' );?>" placeholder="Quantity" name="quantity" id="quantity">
+        <input class="form-control mr-2" type="number" value="<?php echo ( $hours ? $hours : '' );?>" placeholder="# of hours" name="quantity" id="quantity">
         <button class="btn btn-outline-primary" name="submit" type="submit">Submit</button>
       </form>
       <?php 
-        if($coffee){
-           echo $coffee;
+        if($activity){
+           echo $activity;
         }
       ?>
     </main>
